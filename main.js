9facd987ac6072101dc30095aa7bba1b49783f30
rohan-star7308 <87434006+rohan-star7308@users.noreply.github.com>
2022-02-10 20:33:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version=",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/iJtvv7sje/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded!!!");
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    var speak_data_1="the first prediction is"+prediction_1;
+    var utterThis= new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+
+function check(){
+    img=document.getElementById("captured_image")
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_1").innerHTML=results[0].label;
+        prediction_1=results[0].label;
+        speak();
+        
+    }
+}
